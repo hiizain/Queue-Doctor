8277549b7b98e42c0f18c1f9d7ec260f27fd9ea7
hiizain <hizain512@gmail.com>
2022-07-10 11:22:06 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
-<?php
-header("Location: e-healt/landing/landing.php");
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta http-equiv="refresh" content="1;url=e-healt/landing/landing.php">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
